ebeab1dbdd70878c3ce04d5fc168fd8a759a3896
ABRARgomaa <abrargomaa111@gmail.com>
2024-08-26 16:05:40 +0300

    handeling middleware

--- step1/server.js
+++ step1/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorhandler = require('./middleware/errorhandler')
 const PORT = process.env.PORT || 3500
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.json())
+//app.use(cookieParser)
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/', require('./routes/root'))
 
@@ -16,5 +25,6 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 NOT FOUND')
     }
 })
+app.use(errorhandler)
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
