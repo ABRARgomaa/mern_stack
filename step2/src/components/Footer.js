9b400ea31bc21fadedb22f37e1af5233c7bdc701
ABRARgomaa <abrargomaa111@gmail.com>
2024-09-03 02:53:56 +0300

    front

--- step2/src/components/Footer.js
+++ step2/src/components/Footer.js
@@ -9,27 +9,36 @@ const Footer = () => {
                 <div className="footer__item">
                     <FontAwesomeIcon icon={faPhone} />
                     <p>
-                        <a href="https://wa.me/2234333550" className="footer-link">
-                        +20 102 686 7665
+                        <a href="https://wa.me/201026867665" className="footer-link">
+                            +20 102 686 7665
                         </a>
                     </p>
                 </div>
                 <div className="footer__item">
                     <FontAwesomeIcon icon={faEnvelope} />
                     <p>
-                        <a href="mailto:coffeeshop@gmail.com" className="footer-link">
+                        <a href="mailto:abrargomaa111@gmail.com" className="footer-link">
                             abrargomaa111@gmail.com
                         </a>
                     </p>
                 </div>
                 <div className="footer__item">
                     <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    <p>43 Anywhere St. Anycity</p>
+                    <p>
+                        <a 
+                          href="https://www.google.com/maps?q=43+Anywhere+St,+Anycity"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="footer-link"
+                        >
+                            1600 Amphitheatre Parkway, Mountain View, CA 94043, USA
+                        </a>
+                    </p>
                 </div>
             </div>
             <p className='small'>© 2024 Cozy Corner Café. All rights reserved.</p>
         </footer>
-    )
+    );
 }
 
 export default Footer;
