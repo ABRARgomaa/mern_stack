1961f19e26719838677b347a11af0e401aa64c04
ABRARgomaa <abrargomaa111@gmail.com>
2024-09-05 00:46:40 +0300

    UBDATING FRONT END

--- step2/src/features/products/Product.js
+++ step2/src/features/products/Product.js
@@ -2,19 +2,24 @@ import { useSelector } from 'react-redux'
 import { selectProductById } from './productsApiSlice'
 
 const Product = ({ productId }) => {
-    const product = useSelector(state => selectProductById(state, productId))
-
-
+    const product = useSelector((state) => selectProductById(state, productId));
+  
     if (product) {
+      return (
+        <div className="product-card">
+          <div className="product-image">
+            {/* Update the image source to use the correct URL */}
+            <img src={`/products/${product._id}/image`} alt={product.name} />
+          </div>
+          <div className="product-details">
+            <h3 className="product-name">{product.name}</h3>
+            <p className="product-price">${product.price.toFixed(2)}</p>
+            <p className="product-description">{product.description}</p>
+            <p className="product-category">{product.category}</p>
+          </div>
+        </div>
+      );
+    } else return null;
+  };
 
-        return (
-            <tr className="table__row product">
-                <td className="table__cell">{product.name}</td>
-                <td className="table__cell">${product.price.toFixed(2)}</td>
-                <td className="table__cell">{product.category}</td>
-            </tr>
-        )
-
-    } else return null
-}
 export default Product
