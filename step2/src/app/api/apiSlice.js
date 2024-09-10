c338e50818d0630c57f12cfd84f9a2a6de13f94e
ABRARgomaa <abrargomaa111@gmail.com>
2024-09-10 03:48:28 +0300

    up

--- step2/src/app/api/apiSlice.js
+++ step2/src/app/api/apiSlice.js
@@ -16,30 +16,26 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions);
-
+  
     if (result?.error?.status === 403) {
-        console.log('sending refresh token');
-
-        // Call the refresh token endpoint
-        const { data, error } = await baseQuery('/auth/refresh', api, extraOptions);
-
-        if (data) {
-            // Update the token in the Redux store
-            api.dispatch(setCredentials({ ...data }));
-            // Retry the original request with the new token
-            result = await baseQuery(args, api, extraOptions);
-        } else {
-            // If the refresh token request fails, log the user out
-            api.dispatch(logOut());
-            if (error?.status === 403) {
-                error.data.message = "Your login has expired.";
-            }
-            return error;
-        }
+      console.log('sending refresh token');
+  
+      // Call the refresh token endpoint
+      const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+  
+      if (refreshResult?.data) {
+        // Update the token in the Redux store
+        api.dispatch(setCredentials({ ...refreshResult.data }));
+        // Retry the original request with the new token
+        result = await baseQuery(args, api, extraOptions);
+      } else {
+        api.dispatch(logOut());
+        // handle logout - redirect to login page or show a message
+      }
     }
-
+  
     return result;
-}
+  }
 
 export const apiSlice = createApi({
     baseQuery: baseQueryWithReauth,
